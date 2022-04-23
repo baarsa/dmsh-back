d7109b052d91691c9f9a203a206786ce20c6bc36
kharchev <kharchevbv@gmail.com>
2022-04-23 10:52:32 +0300

    added cors headers

--- php/src/index.php
+++ php/src/index.php
@@ -35,6 +35,14 @@ session_start();
 $app = AppFactory::create();
 $app->addBodyParsingMiddleware();
 $app->addErrorMiddleware(false, false, false);
+$app->add(function ($request, $handler) {
+    $response = $handler->handle($request);
+    return $response
+        ->withHeader('Access-Control-Allow-Origin', 'http://localhost:5001')
+        ->withHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Accept, Origin, Authorization')
+        ->withHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS')
+        ->withHeader('Access-Control-Allow-Credentials', 'true');
+});
 
 $db = new Database();
 
